7efd2202e2538a8765e9f58de8d68a7e494c71cb
Abraham Jorge <afjh23@gmail.com>
2024-07-14 23:55:40 -0500

    filtro hecho? rezar que funcione

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import datajs from './assets/stays.json'
 import './App.css'
@@ -6,21 +6,57 @@ import { Modal } from './components/Modal'
 
 function App() {
   const [data, setData] = useState(datajs)
+  const [stay, setStay] = useState(datajs)
   const [openModal,setOpenModal] = useState(false)
+  const [locations, setLocations] = useState("")
+  const [guests, setGuests] = useState("")
+  const [guestsValues, setGuestsValues] = useState({ adults: 0, children: 0 })
 
   function toggleModal(){
     setOpenModal(!openModal)
   }
+
+
+  function handleFilter(){
+   
+    
+    if(locations===""&&guests===""){
+      setStay(data)
+    }else{
+
+      const citylocation= locations.split(",")
+      console.log(citylocation)
+    const filteredData = data.filter(stay => {
+      if(locations === '' && guests == 0){
+        return true
+      } else if(locations !== '' && guests == 0){
+        return stay.city == citylocation[0]
+      }else if(locations === '' && guests > 0){
+        console.log(citylocation+" "+guests)
+        return stay.maxGuests >= guests
+      }else{
+        return stay.city == citylocation[0] && stay.maxGuests >= guests
+      }
+    })
+
+    setStay(filteredData)}
+
+  }
+
+  useEffect(()=>{
+    handleFilter()
+  }, [locations, guests])
+
   return (
     <>
-      {openModal && <Modal toggleModal={toggleModal}></Modal>}
+      {openModal && <Modal toggleModal={toggleModal} data={data} setStay={setStay} setLocations={setLocations} guests={guests} setGuests={setGuests} locations={locations} setGuestsValues={setGuestsValues} guestsValues={guestsValues}></Modal>}
       <div className='container'>
         <header>
           <figure><img src='logo.png' /></figure>
-          <div className='search'>
-            <input className='input-location' placeholder='Add Location' />
-            <input className='input-guest' placeholder='Add Guest' />
-            <figure className='search-lupa' onClick={toggleModal}><img src='lupa.svg'></img></figure>
+          <div className='search'  onClick={toggleModal}>
+            <input className='input-location' value={locations} placeholder='Add Location' />
+            <input className='input-guest' placeholder='Add Guest'  value={guests}/>
+            <figure className='search-lupa'><img src='lupa.svg'></img></figure>
           </div>
         </header>
         <main>
@@ -28,7 +64,7 @@ function App() {
             <h1> Stays in </h1>
             <ul>
 
-              {data?.map((x, i) =>
+              {stay?.map((x, i) =>
               (<li key={i}>
                 <article>
                   <figure>
@@ -41,8 +77,8 @@ function App() {
                       <div className='beds'>  <p>.{x.beds}</p></div>
                     </div>
                     <div className='punctuation'>
-                      <span class="rating-star">★</span>
-                      <span class="rating"> {x.rating}</span>
+                      <span className="rating-star">★</span>
+                      <span className="rating"> {x.rating}</span>
                     </div>
                   </div>
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,61 +1,90 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState, Children } from 'react';
 import './modal.css';
 
-export const Modal = ({ toggleModal }) => {
-  const modalRef = useRef(null);
+export const Modal = ({ data, locations, guests, setLocations, setGuests, toggleModal, guestsValues,setGuestsValues }) => {
+    const modalRef = useRef(null);
+    const locationCountry = [...new Set(data.map(guest => `${guest.city},${guest.country}`))].map(guest => guest.split(','))
 
- const handleClickOutside = (e) =>{
-    if(modalRef.current && !modalRef.current.contains(e.target))
-    toggleModal()
- }
+    const handleClickOutside = (e) => {
+        if (modalRef.current && !modalRef.current.contains(e.target))
+            toggleModal()
+    }
+
+    const handleLocation = (e) => {
+        const liClick = e.target.closest('li')
+        setLocations(liClick.querySelector('span').textContent)
+        /*  console.log(e.target.closest('li').id) */
+
+       /*  const rs = data.filter(stay => (stay.city.toLowerCase()) === liClick.id.toLowerCase())
+        console.log(rs)
+        setStay(rs) */
+    }
+
+    const handleGuests = (type, value) => {
+        if ((guestsValues[type] || 0) + value >= 0) {
+            setGuestsValues((prevGuestsValues) => {
+                const newGuestsValues = { ...prevGuestsValues, [type]: prevGuestsValues[type] + value };
+                setGuests(newGuestsValues.adults + newGuestsValues.children);
+                return newGuestsValues;
+              });
+        }
+
+    }
 
 
-  return (
-    <div className='modal' onClick={handleClickOutside}>
-      <div className='modal-content' ref={modalRef} onClick={(e) => e.stopPropagation()}>
-        <div className='modal-inputs'>
-          <div className='modal-location'>
-            <span>LOCATION</span>
-            <input placeholder='Add Location' />
-          </div>
-          <div className='modal-guests'>
-            <span>GUESTS</span>
-            <input placeholder='Add Location' />
-          </div>
-          <button className='search-button'>
-            <img src='lupa.svg' alt='search' />
-            <span>Search</span>
-          </button>
-        </div>
-        <div className='modal-values'>
-          <div className='values-location'>
-            <ul>
-              <li>
-                <figure>
-                  <img src='ubi.svg' alt='location' />
-                </figure>
-                <span> muestra</span>
-              </li>
-            </ul>
-          </div>
-          <div className='values-guests'>
-            <p className='guests-type'>Adults</p>
-            <p className='guests-age'>Ages 13 or above</p>
-            <div>
-              <button>-</button>
-              <span>0</span>
-              <button>+</button>
+    return (
+        <div className='modal' onClick={handleClickOutside}>
+            <div className='modal-content' ref={modalRef} onClick={(e) => e.stopPropagation()}>
+                <div className='modal-inputs'>
+                    <div className='modal-location'>
+                        <span>LOCATION</span>
+                        <input placeholder='Add Location' value={locations} />
+                    </div>
+                    <div className='modal-guests'>
+                        <span>GUESTS</span>
+                        <input placeholder='Add Guests' value={guests}/>
+                    </div>
+                    <button className='search-button' onClick={toggleModal}>
+                        <img src='lupa.svg' alt='search' />
+                        <span>Search</span>
+                    </button>
+                </div>
+                <div className='modal-values'>
+                    <div className='values-location'>
+                        <ul>
+                            {
+                                locationCountry.map(city =>
+                                    <li key={city[0]} id={city[0]} onClick={handleLocation}>
+                                        <figure>
+                                            <img src='ubi.svg' alt='location' />
+                                        </figure>
+                                        <span>{city[0]}, {city[1]}</span>
+                                    </li>
+
+                                )
+                            }
+
+
+                        </ul>
+                    </div>
+                    <div className='values-guests'>
+                        <p className='guests-type'>Adults</p>
+                        <p className='guests-age'>Ages 13 or above</p>
+                        <div>
+                            <button onClick={() => handleGuests('adults', -1)}>-</button>
+                            <span>{guestsValues.adults}</span>
+                            <button onClick={() => handleGuests('adults', +1)}>+</button>
+                        </div>
+                        <p className='guests-type'>Children</p>
+                        <p className='guests-age'>Ages 2 - 12</p>
+                        <div>
+                            <button onClick={() => handleGuests('children', -1)}>-</button>
+                            <span>{guestsValues.children}</span>
+                            <button onClick={() => handleGuests('children', +1)}>+</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <p className='guests-type'>Children</p>
-            <p className='guests-age'>Ages 2 - 12</p>
-            <div>
-              <button>-</button>
-              <span>0</span>
-              <button>+</button>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
